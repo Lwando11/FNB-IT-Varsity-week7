a18e853a0bd586411e915a48b7502ff8dad175c8
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:39:01 +0200

    Create moreloops.py

---
+++ moreloops.py
@@ -0,0 +1,24 @@
+fruitss = ["Apple", "Pear", "Lemon", "Grape"]
+for fruit1 in fruitss:
+
+ if fruit1 == "Lemon":
+    break  #Stops the loop
+ print(fruit1)
+ 
+ 
+ print()
+ 
+ 
+kars = ["Volvo", "VW", "BMW", "BYD"]
+for kar in kars:
+    if kar == "BMW":  
+     continue #Skips BMW and moves to the next iteration
+     print(kar)
+
+print()
+
+numbers1 = [1, 3, 5, 6, 8]
+for number1 in numbers1:
+    if number1 == 3:
+      pass  
+      print(number1)
