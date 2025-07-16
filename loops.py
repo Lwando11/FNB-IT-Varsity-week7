01215e5be9c3a1ee905042201ed5a4d9066a86cf
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:38:18 +0200

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,30 @@
+#Loops
+
+fruits = ["Apple", "Pear", "Lemon", "Grape"]
+
+for fruit in fruits:
+ print(fruit)
+ 
+ cars = ["Volvo", "VW", "BMW", "BYD"]
+for car in cars:
+    print(car)
+
+numbers = [1, 3, 5, 6, 8]
+for number in numbers:
+    print(number)
+    
+
+count = 3
+
+while count <=12:
+    print(count)
+    count += 1
+    
+counter = 27
+
+while counter >=12:
+    print(counter)
+    counter += 4
+    if counter == 117831:
+     break
+    
