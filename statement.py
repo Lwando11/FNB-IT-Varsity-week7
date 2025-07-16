d27b6930b19bd56118dfd7c15426f1abff2d800c
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:40:51 +0200

    Create statement.py

---
+++ statement.py
@@ -0,0 +1,22 @@
+
+num = 24 
+
+if num > 0 :
+    print("This is a positive number")
+else:
+    print("This is a negative number")
+    
+    
+
+    
+    
+num2 = 39
+
+if num2 < 13:
+    print("this number is less than") 
+
+elif num2 == 39:
+    print("Perfect number")
+
+else:
+    print("this number is greater")
