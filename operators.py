0876defc558eabd81d793642da899f165a44d794
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:39:45 +0200

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,25 @@
+#operators
+
+#addition(+)
+#subtraction(-)
+#division(/)
+#multiplication(*)
+
+
+n = 10 
+m = 2
+print(n+m)
+print(n-m)
+print(n/m)
+print(n*m)
+
+
+y = 45
+y += 23
+print(y)
+y -= 18
+print(y)
+y /= 5 
+print(y)
+y *= 10
+print(y)
