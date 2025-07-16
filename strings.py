62df7425ac4d2bd8ccd1fec4102ca82a3dc9e46e
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:41:46 +0200

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,42 @@
+#string 
+
+message = "Hello world"
+
+print(message)
+
+messages = """Bob's world
+is cool"""
+
+print(messages)
+
+checking = "Brothers"
+
+print(len(checking))
+
+print(checking[-1])
+print(checking[0])
+print(checking[4])
+
+lines = "We, are doing Python"
+
+print(lines.split())
+print(lines.upper())
+print(lines[4].upper())
+print(lines.strip())
+messagess = "we love doing python"
+new_message = messagess.replace("python", "JavaScript")
+print(new_message)
+
+new_lines = lines.replace("are", "love")
+print(new_lines)
+
+#variables
+
+#valid variables
+
+user = "John"
+my_variable = 10
+
+
+#invalid variables 
+ 2nd variable = 30
