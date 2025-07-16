fa06fd520dc3b9b27d3e7becf4ce79949f1ab880
Lwando Bhixa <134373350+Lwando11@users.noreply.github.com>
2025-07-16 16:37:15 +0200

    Create controls.py

---
+++ controls.py
@@ -0,0 +1,9 @@
+#controls
+num3 = int(input("Place your number: "))
+num4 = int(input("Place your number: "))
+
+if num3 > num4 :
+    print("This number is greater", num4)
+elif num4 > num3 :
+    print("This number is greater", num3)
+else: ("both numbers are the same")
